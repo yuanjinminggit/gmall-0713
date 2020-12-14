72c4c9d7d71f83d207f38f35294c67cbf2772685
yuanjinminggit <1779569744@qq.com>
2020-12-14 20:02:45 +0800

    项目初始化

---
+++ gmail-pms/src/main/java/com/atguigu/gmail/pms/GmailPmsApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmail.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmailPmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmailPmsApplication.class, args);
+    }
+
+}
---
+++ gmail-pms/src/test/java/com/atguigu/gmail/pms/GmailPmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmail.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmailPmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
